4e396b87d43494412ab3f8388837491cdbd3048a
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-11-27 20:11:11 +0100

    tasks

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let [value, idx] of array.entries()) {
-    array[idx] = appendString + value;
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
 
-  return array;
+  return newArray;
 }
