73b4d792b11047996c20e1e63e5accaab207b659
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-12-01 15:36:30 +0100

    task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,35 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || conversionRate !== 'number') throw new Error('amount and conversion rate must be an integer');
+    return amount * conversionRate;
+  }
+
+  set amount(v) {
+    if (typeof v !== 'number') throw new Error('amount must be an integer');
+    this._amount = v;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(v) {
+    if (!(v instanceof Currency)) throw new Error('currentcy must be a type of Currency');
+    this._currency = v;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+}
