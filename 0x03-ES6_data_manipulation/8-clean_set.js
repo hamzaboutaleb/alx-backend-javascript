294efb1c5601fcab5869d00187ecb3b042caf578
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-12-05 19:47:38 +0100

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
   if (startString === undefined || startString.length === 0) return '';
   const arr = Array.from(set);
-  return arr.filter((s) => s.startsWith(startString)).map(s => s.slice(startString.length)).join('-');
+  return arr.filter((s) => s.startsWith(startString)).map((s) => s.slice(startString.length)).join('-');
 }
