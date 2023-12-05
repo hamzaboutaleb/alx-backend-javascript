a22f20e5315d48f645b27225ed1afe58bc389950
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-12-05 19:09:46 +0100

    task 4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,16 @@
+export default function updateStudentGradeByCity(students, city, grades) {
+  const gradesList = {};
+  grades.forEach(({ studentId, grade }) => {
+    gradesList[studentId] = grade;
+  });
+  return students.filter((student) => student.location === city).map((student) => {
+    let grade = null;
+    if (student.id in gradesList) {
+      grade = gradesList[student.id];
+    }
+    return {
+      ...student,
+      grade,
+    };
+  });
+}
